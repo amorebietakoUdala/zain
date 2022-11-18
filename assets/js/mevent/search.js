5e90bacf5f0e6c2856c3b3a297701a2c06c27f04
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2022-11-18 11:06:08 +0100

    - Changed datepicker due to vulnerabilities. - Updated dependencies due to vulnerabilities.

--- assets/js/mevent/search.js
+++ assets/js/mevent/search.js
@@ -5,11 +5,9 @@ import $ from 'jquery';
 import 'bootstrap-table';
 import 'bootstrap-table/dist/locale/bootstrap-table-es-ES';
 import 'bootstrap-table/dist/locale/bootstrap-table-eu-EU';
-import 'bootstrap-datepicker';
-import 'bootstrap-datepicker/js/locales/bootstrap-datepicker.es';
-import 'bootstrap-datepicker/js/locales/bootstrap-datepicker.eu';
-import 'eonasdan-bootstrap-datetimepicker';
-import 'pc-bootstrap4-datetimepicker';
+import tempusDominus from '@eonasdan/tempus-dominus';
+import customDateFormat from '@eonasdan/tempus-dominus/dist/plugins/customDateFormat';
+
 
 function View() {
   var dom = {
@@ -48,11 +46,30 @@ function View() {
 }
 
 $(document).ready(function(){
-    $('.js-datetimepicker').datetimepicker({
-		format: 'YYYY-MM-DD HH:mm',
-		sideBySide: true,
-		locale: $('html').attr('lang'),
-	});
+	let current_locale = $('html').attr('lang') + '-' + $('html').attr('lang').toUpperCase();
+	tempusDominus.extend(customDateFormat);
+	const options = {
+		display: {
+			buttons: {
+				close: true,
+				clear: true,
+			},
+			components: {
+				decades: false,
+				year: true,
+				month: true,
+				date: true,
+				clock: true,
+			},
+		},
+		localization: {
+			locale: current_locale,
+			dayViewHeaderFormat: { month: 'long', year: 'numeric' },
+			format: 'yyyy-MM-dd HH:mm',
+		},
+	};
+	let datepicker1 = new tempusDominus.TempusDominus(document.getElementById('event_search_form_dateFrom'), options);
+	let datepicker2 = new tempusDominus.TempusDominus(document.getElementById('event_search_form_dateTo'), options);
 	$('.js-showDetails').on('click',function(e){
 		e.preventDefault();
 		var id = e.currentTarget.id;
@@ -63,11 +80,11 @@ $(document).ready(function(){
 	$('.close').click(function(e) {
 		var number = e.target.id.slice(2);
 		$('#ov'+number).fadeOut(300);
-	    $('.bootstrap-table').show();
+		$('.bootstrap-table').show();
 	});
 	$('.overlay').hide();
 	$('#taula').bootstrapTable({
-	    cache : false,
+		cache : false,
 		showExport: false,
 		exportDataType: 'all',
 		showColumns: false,
@@ -84,8 +101,8 @@ $(document).ready(function(){
 	var $table = $('#taula');
 	$(function () {
 	    $('#toolbar').find('select').change(function () {
-		$table.bootstrapTable('destroy').bootstrapTable({
-		    exportDataType: $(this).val(),
+			$table.bootstrapTable('destroy').bootstrapTable({
+			exportDataType: $(this).val(),
 		});
 	    });
 	});
@@ -103,6 +120,6 @@ $(document).ready(function(){
 	$('.close').click(function(e) {
 		var number = e.target.id.slice(2);
 		$('#ov'+number).fadeOut(300);
-	    $('.bootstrap-table').show();
+		$('.bootstrap-table').show();
 	});
 });
