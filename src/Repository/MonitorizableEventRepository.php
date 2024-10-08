ac782fd4c9739c811b23ca4354ad6df76e850625
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2024-10-08 07:55:08 +0200

    Updated some dependencies due to vulnerabilities

--- src/Repository/MonitorizableEventRepository.php
+++ src/Repository/MonitorizableEventRepository.php
@@ -9,10 +9,10 @@ use Symfony\Component\Security\Core\User\User;
 use App\Entity\MonitorizableEvent;
 
 /**
- * @method User|null find($id, $lockMode = null, $lockVersion = null)
- * @method User|null findOneBy(array $criteria, array $orderBy = null)
- * @method User[]    findAll()
- * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method MonitorizableEvent|null find($id, $lockMode = null, $lockVersion = null)
+ * @method MonitorizableEvent|null findOneBy(array $criteria, array $orderBy = null)
+ * @method MonitorizableEvent[]    findAll()
+ * @method MonitorizableEvent[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class MonitorizableEventRepository extends ServiceEntityRepository
 {
